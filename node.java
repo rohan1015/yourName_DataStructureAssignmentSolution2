e6a7d5f54f07b63da9f65a812bf0ae2dac4a3945
rohan1015 <53273630+rohan1015@users.noreply.github.com>
2023-03-23 16:25:36 +0530

    Create node.java

---
+++ node.java
@@ -0,0 +1,12 @@
+package datastructureassignmentsolution2;
+
+public class Node {
+	public int data;
+	public Node left, right;
+
+	public Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
